af20ba7bfdfd049a36f5be3f5486e5abdf54e511
Satsathish132 <satsathish407@gmail.com>
2026-07-14 16:13:38 +0530

    Add .gitignore for Spring Boot project

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,16 +1,28 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT || 4000, // TiDB Cloud default port is 4000
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: {
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true,
+  },
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.connect((err) => {
-  if (err) console.log("DB Error", err);
-  else console.log("MySQL Connected");
+db.getConnection((err, connection) => {
+  if (err) {
+    console.log("DB Error", err);
+  } else {
+    console.log("MySQL Connected");
+    connection.release();
+  }
 });
 
 module.exports = db;
